cea8814acce3e2274c465f4f227f27ab91a61490
VitorOliv <38124257+VitorOliv@users.noreply.github.com>
2025-09-03 02:22:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -24,6 +24,19 @@ const toggleAccorddion = (e) => {
 
         button.innerHTML = "+";
     }
-    
-    
 }
+
+
+// Slick
+
+$(document).ready(function(){
+  $(".carousel").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,  
+    variableWidth: true,
+    centerMode: true,
+  centerPadding: '60px',
+  });
+});
